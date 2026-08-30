ca5438eee308823310139ad51959bcb1a5cf468c
udaykumar-alt <udaykumar207207@gmail.com>
2026-08-30 22:30:00 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-#!/bin/bash 
-kubectl apply -f k8s/deployment.yaml 
-kubectl apply -f k8s/service.yaml 
-kubectl apply -f k8s/secrets.yaml 
-kubectl apply -f k8s/namespace.yaml 
+#!/bin/bash
+set -e
+kubectl apply -f k8s/namespace.yaml
+kubectl apply -f k8s/secrets.yaml
+kubectl apply -f k8s/deployment.yaml
+kubectl apply -f k8s/service.yaml
